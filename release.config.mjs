a95fdd3a01dd555a3389b08aed18d9b13ce5b931
Danilo Pianini <danilo.pianini@unibo.it>
2024-08-01 15:43:50 +0200

    ci(semantic-release): configure semantic-release

---
+++ release.config.mjs
@@ -0,0 +1,8 @@
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
+config.plugins = [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+    "@semantic-release/git",
+];
+export default config;
