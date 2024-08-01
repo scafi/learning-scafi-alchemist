c6ec76ee209dd7468c4d66a5230f76bf1adacfc4
Danilo Pianini <danilo.pianini@unibo.it>
2024-08-01 13:35:37 +0200

    ci(semantic-release): manually configure plugins to disable npm

--- release.config.mjs
+++ release.config.mjs
@@ -1,5 +1,7 @@
 import config from 'semantic-release-preconfigured-conventional-commits' assert { type: "json" };
-config.plugins.push(
+config.plugins = [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     "@semantic-release/github",
     "@semantic-release/git",
-);
+];
