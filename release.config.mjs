59f606586beabd626b9069a6e0a8e025411c9a02
Danilo Pianini <danilo.pianini@unibo.it>
2025-03-04 19:19:32 +0100

    Delete release.config.mjs

--- release.config.mjs
+++
@@ -1,7 +0,0 @@
-import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
-config.plugins = [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/github",
-];
-export default config;
