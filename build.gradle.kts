6fae7345f1da5d8aedec998f6ec1018c8d345eea
Gianluca Aguzzi <gianluca.aguzzi@unibo.it>
2025-03-04 19:02:19 +0100

    fix(build): align with alchemist primer build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.internal.extensions.stdlib.capitalized
 
 
 plugins {
@@ -22,7 +23,6 @@ multiJvm {
 
 val batch: String by project
 val maxTime: String by project
-val variables: String by project
 
 val alchemistGroup = "Run Alchemist"
 /*
@@ -36,64 +36,45 @@ val runAll by tasks.register<DefaultTask>("runAll") {
  * Scan the folder with the simulation files, and create a task for each one of them.
  */
 File(rootProject.rootDir.path + "/src/main/yaml").listFiles()
-    .filter { it.extension == "yml" } // pick all yml files in src/main/yaml
-    .sortedBy { it.nameWithoutExtension } // sort them, we like reproducibility
-    .forEach { it ->
+    ?.filter { it.extension == "yml" } // pick all yml files in src/main/yaml
+    ?.sortedBy { it.nameWithoutExtension } // sort them, we like reproducibility
+    ?.forEach {
         // one simulation file -> one gradle task
-        val task by tasks.register<JavaExec>("run${it.nameWithoutExtension.capitalize()}") {
+        val task by tasks.register<JavaExec>("run${it.nameWithoutExtension.capitalized()}") {
             group = alchemistGroup // This is for better organization when running ./gradlew tasks
             description = "Launches simulation ${it.nameWithoutExtension}" // Just documentation
             mainClass.set("it.unibo.alchemist.Alchemist") // The class to launch
             classpath = sourceSets["main"].runtimeClasspath // The classpath to use
-            // In case our simulation produces data, we write it in the following folder:
-            val exportsDir = File("${projectDir.path}/build/exports/${it.nameWithoutExtension}")
-            doFirst {
-                // this is not executed upfront, but only when the task is actually launched
-                // If the export folder doesn not exist, create it and its parents if needed
-                if (!exportsDir.exists()) {
-                    exportsDir.mkdirs()
-                }
-            }
             // Uses the latest version of java
             javaLauncher.set(
                 javaToolchains.launcherFor {
-                    languageVersion.set(JavaLanguageVersion.of(multiJvm.latestJava))
-                }
+                    languageVersion.set(JavaLanguageVersion.of(17))
+                },
             )
             // These are the program arguments
-            args("run", it.absolutePath)
+            args("run", it.absolutePath, "--override")
             if (System.getenv("CI") == "true" || batch == "true") {
                 // If it is running in a Continuous Integration environment, use the "headless" mode of the simulator
                 // Namely, force the simulator not to use graphical output.
                 args(
-
-                    "--override",
-                    "terminate: { type: AfterTime, parameters: [$maxTime] }",
-                    "--override",
                     """
-                    launcher: {
-                        parameters: {
-                            batch: [ ],
-                            showProgress: true,
-                            autoStart: true,
-                            parallelism: 1,
-                        }
-                    }
-                """.trimIndent())
+                        terminate:
+                        - type: AfterTime
+                          parameters: $maxTime
+                    """.trimIndent(),
+                )
             } else {
                 // A graphics environment should be available, so load the effects for the UI from the "effects" folder
                 // Effects are expected to be named after the simulation file
                 args(
-                    "--override",
-                    "monitors: { type: SwingGUI, parameters: { graphics: effects/${it.nameWithoutExtension}.json } }",
-                    "--override",
-                    "launcher: { parameters: { batch: [], autoStart: false } }",
-                    "--override",
-                    "terminate: { type: AfterTime, parameters: [$maxTime] }"
+                    """
+                        monitors:
+                          type: SwingGUI
+                          parameters:
+                            graphics: effects/${it.nameWithoutExtension}.json
+                    """,
                 )
             }
-            // This tells gradle that this task may modify the content of the export directory
-            outputs.dir(exportsDir)
         }
         // task.dependsOn(classpathJar) // Uncomment to switch to jar-based classpath resolution
         runAll.dependsOn(task)
