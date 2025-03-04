20fed230dc81d1a7d85d1c6814b1e1393c8e689d
Danilo Pianini <danilo.pianini@unibo.it>
2025-03-04 19:13:12 +0100

    ci: add the foojay convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("com.gradle.enterprise") version "3.18.1"
+    id("com.gradle.develocity") version "3.19.2"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
-
-gradleEnterprise {
+develocity {
     buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishOnFailure()
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
     }
 }
 
